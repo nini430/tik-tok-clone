70bf37d9cb7d82e8fa444b7c891038483b31d6d6
ngogatishvili <ngogatishvili@websmart.gmail.com>
2023-04-17 10:39:12 +0400

    initialize navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='w-full flex justify-between items-center px-4 py-2 border-b border-gray-200'>
+        <div>
+          <Link href='/'>
+          <Image src='/assets/tiktik-logo.png' alt='tiktik' width={100} height={100} />
+          </Link>
+        </div>
+    </div>
   )
 }
 
-export default Navbar
+export default Navbar;
