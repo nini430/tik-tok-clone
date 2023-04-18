9f179eea429669c677fb413370cfbb74a0020ae3
ngogatishvili <ngogatishvili@websmart.gmail.com>
2023-04-18 12:49:43 +0400

    google oauth@latest

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,13 +1,19 @@
 import Image from "next/image";
 import Link from "next/link";
+import {GoogleLogin,googleLogout} from '@react-oauth/google'
 
 const Navbar = () => {
+  const user=false;
   return (
     <div className='w-full flex justify-between items-center px-4 py-2 border-b border-gray-200'>
         <div>
           <Link href='/'>
           <Image src='/assets/tiktik-logo.png' alt='tiktik' width={100} height={100} />
           </Link>
+        </div>
+        <div>SEARCH</div>
+        <div>
+          {user? <div>Logged in</div>: <GoogleLogin onSuccess={response=>console.log(response)} onError={()=>console.log('Error')}/>}
         </div>
     </div>
   )
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import Sidebar from '@/components/Sidebar';
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import {useState,useEffect} from 'react'
+import {GoogleOAuthProvider} from '@react-oauth/google'
 
 export default function App({ Component, pageProps }: AppProps) {
   const [isSSR,setIsSSR]=useState(true);
@@ -14,7 +15,8 @@ export default function App({ Component, pageProps }: AppProps) {
 
   if(isSSR) return null;
   return (
-    <div>
+    <GoogleOAuthProvider clientId={`${process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}`}>
+      <div>
       <Navbar/>
        <div className='flex gap-6 md:gap-20'>
         <div className='h-[92vh] overflow-hidden xl:hover:overflow-auto'>
@@ -25,6 +27,8 @@ export default function App({ Component, pageProps }: AppProps) {
         </div>
     </div>
     </div>
+    </GoogleOAuthProvider>
+    
    
   )
 }
